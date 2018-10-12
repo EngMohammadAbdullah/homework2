428413156bf5c9b802cfa9a647f6b0540e3dbc80
prabhamanju <43313631+prabhamanju@users.noreply.github.com>
2018-10-13 00:05:12 +0200

    there are few functions which need to be implementded. still trying to do in best possible way to do so. sorry for delay.
    I have implemented code as per my understanding. please review the code and send me feedback for improvement.

---
+++ index.js
@@ -0,0 +1,340 @@
+var homeWork = homeWork || {};
+
+
+homeWork = function () {
+
+}
+
+homeWork.prototype.print_hello_world = function () {
+    //Here an example for the homework 
+
+    var str = "Hello";
+    return str;
+
+    //document.write(str);
+}
+
+//Write a program that asks the user for a number n and prints the sum of the numbers 1 to n
+
+homeWork.prototype.add_1_to = function (n) {
+    //code here
+//manju
+	var sum = 0;
+	for (var i = 0; i <= n; i++){
+
+	sum += i;}
+	return sum;
+    throw new Error("Not Implemneted");
+}
+
+
+
+//Write a program that asks the user for a number n and gives them the possibility to choose between computing the sum(+) or  computing the multipe (*)  of 1, â€¦, n.
+
+homeWork.prototype.choose_operation = function (n, operation) {
+
+    //code here
+    //manju 
+    /*var operation;
+    var n;
+    switch(operation){
+        case '+':
+
+        for (var i = 0; i <= n; i ++){
+            n += 1;
+        }
+        break;
+        case '*':
+        for (var i = 0; i <= n; i ++){
+            n *= 1;
+        }
+        break;
+        default:
+            return console.log("only choose + or *");
+        return n;
+    }*/
+    throw new Error("Not Implemneted");
+
+}
+
+//Write a function that returns the largest element in a list
+homeWork.prototype.get_max_number = function (list) {
+    /* 
+      var list = [1,5,8,1,19,3,8]
+     max here 19
+     */
+    //code here
+    return list[2];
+    throw new Error("Not Implemneted");
+}
+
+//Write function that reverses a list, preferably in place.
+
+homeWork.prototype.reverse_list = function (list) {
+    /*
+     list = ['a',4,8,'r']
+     after reverse_list(list)
+     ['r',8,4,'a']
+     */
+//manju
+//list = ['a', 4,8,'r']
+//function reverse(list){
+var reversed =list.reverse();
+return reversed;
+    throw new Error("Not Implemneted");
+}
+
+//Write a function that checks whether an element exist in a list.
+
+homeWork.prototype.Is_Exist = function (element, list) {
+    /*
+     element ex:5
+     list = [1,5,8,6,9,7]
+     return true ; 
+     element ex:11
+     list = [1,5,8,6,9,7]
+      return false ;
+     */
+
+    throw new Error("Not Implemneted");
+
+}
+
+//Write a function that returns the elements on odd positions in a list.
+
+homeWork.prototype.get_odd_position_elements = function (list) {
+    /*
+     list =  [1,5,2,3,4,9,10,12]
+     return 5,3,9,12
+     */
+    // manju
+    var oddPosition = [];
+    for (i = 0; i < list.length; i = i + 2) {
+        oddPosition.push(list[i]);
+    }
+    return oddPosition;
+
+    throw new Error("Not Implemneted");
+
+}
+
+
+//Write a function that returns the odd elements  in a list.
+
+homeWork.prototype.get_odd_numbers = function (list) {
+    /*
+     list =  [1,5,2,3,4,9,10,12]
+     return 1,5,3,9
+     */
+    throw new Error("Not Implemneted");
+
+}
+
+
+//Write a function that concatenates two lists. [a,b,c], [1,2,3]  To [a,b,c,1,2,3]
+
+/*homeWork.prototype.list_concatenate = function (list1, list2) {
+  var list2 = [];
+  var list1 = [];
+  list1 = list1.length;
+  list2 = list2.lenght;
+  list1 = list1.concat(list2);
+  //var i = 0;
+   //while(i < len){
+ // list1[list1.lenght] = list2[i++]
+} 
+  return list1;
+
+   throw new Error("Not Implemneted");
+}*/
+
+/*homeWork.prototype.list_concatenate = function list_concatenate (list1, list2) {
+function list_concatenate (list2, list1) {
+
+var concat_list = list2.concat(list1);
+return list;
+}
+//list2 = list_concatenate (list2, list1);
+
+//console.log(list2);
+
+   throw new Error("Not Implemneted");
+}*/
+
+homeWork.prototype.list_concatenate = function list_concatenate (list1, list2) {
+
+var concatedList
+= list2.concat(list1);
+
+return concatedList;
+
+ throw new Error("Not Implemneted");
+
+}
+
+//Write a function that takes a number and returns a list of its digits. So for 2342 it should return [2,3,4,2].
+
+/*homeWork.prototype.convert_number_to_list = function (number) {
+
+
+    throw new Error("Not Implemneted");
+
+}*/
+//added by manju 
+
+
+homeWork.prototype.convert_number_to_list = function (number) {
+  var xyz = number.toString();
+var convert_number_to_list = xyz.split("");
+return convert_number_to_list; 
+
+throw new Error("Not Implemneted");
+
+}
+
+/*
+ Write a function that takes a list of strings an prints them, one per line, in a rectangular frame.For example the list["Hello", "World", "in", "a", "frame"] gets printed as:
+
+*********
+* Hello *
+* World *
+* in    *
+* a *
+* frame *
+*********
+*/
+
+homeWork.prototype.string_decorate = function (str) {
+    /*var list = str.split(" ")
+    list.forEach(function(array){
+        console.log("*" + array + "*");
+        }) */      
+
+    throw new Error("Not Implemneted");
+
+}
+
+/*
+Write a JavaScript program where the program takes a random integer between 1 to 10, the user is then prompted to input a guess number.If the user input matches with guess number, the program will display a message "Good Work" otherwise display a message "Not matched"
+*/
+
+homeWork.prototype.guess_number = function (user_number) {
+    const num = Math.ceil(Math.random(10) * 10);
+    //console.log(num);
+// manju 
+    const gnum = prompt('Guess the number between 1 and 10 inclusive');
+    //console.log(gnum);
+        if (gnum == num){
+            return ("Good Work");}
+        else{
+            return ("Not matched");}
+    throw new Error("Not Implemneted");
+
+}
+
+/*
+  Write a JavaScript program to get the difference between a given number and 13, if the number is greater than 13 return double the absolute difference
+ */
+
+homeWork.prototype.difference = function (user_number) {
+// manju 
+
+    var difference = (user_number <= 13) ? 13 - user_number : (user_number - 13) * 2;
+    return difference;
+
+    throw new Error("Not Implemneted");
+
+}
+
+/*
+  Write a JavaScript program to create a new string from a given string with the first character of the given string added at the front and back.
+  ex:hack becomes : HhackH
+ */
+
+homeWork.prototype.add_char_to = function (givenString) {
+// manju 
+  
+  var first_char = givenString.charAt(0);
+   var char = first_char.toUpperCase();
+    var add_char_to = char + givenString + char;
+    return add_char_to; 
+
+    throw new Error("Not Implemneted");
+
+}
+
+/*
+ Write a JavaScript program to create a new string from a given string taking the last 3 characters and added at both the front and back. The string length must be 3 or more.
+ */
+
+homeWork.prototype.add_last_three_char_to = function (givernStr) {
+//manju 
+
+    if (givernStr.length < 3){ return false ;}
+        //console.log("please enter string having at least 3 characters")}
+    else var newstring = givernStr.substring(givernStr.length - 3);
+    var add_last_three_char_to = newstring + givernStr + newstring;
+
+    return add_last_three_char_to;
+
+    throw new Error("Not Implemneted");
+
+}
+
+
+/*
+ 
+ Write a JavaScript program to check if three given integer values are in the range 50..99 (inclusive). Return true if one or more of them are in the said range.
+ */
+homeWork.prototype.check_number_in_range = function (number1, number2, number3) {
+
+// manju 
+if (number1 >=50 & number1<= 99){
+    return true;
+}
+else {
+   return false;
+}
+if (number2 >=50 & number2 <= 99){
+    return true;
+}else {
+    return false;
+ }
+if (number3 >=50 & number3<= 99){
+    return true;
+}else {
+    return false;
+ }
+ return check_number_in_range;
+
+    throw new Error("Not Implemneted");
+
+}
+
+/*
+ 
+    Write a JavaScript program to check if the last digit of the three given positive integers is same.
+
+ 
+ */
+homeWork.prototype.Is_last_digit_positive = function (number1, number2, number3) {
+    /*
+     
+     311,221,21
+     1 ,1,1 :  true 
+
+    31,27,53
+     1 ,7 ,3 :false
+     */
+
+     // manju 
+     if ((number1 > 0) && (number2 > 0) && (number3 > 0)){
+         return (number1 % 10 == number2 % 10 && number2 % 10 == number3 % 10 && number1 % 10 == number3 % 10);
+     }
+     else 
+     return false;
+
+    throw new Error("Not Implemneted");
+
+}
+module.exports = homeWork;
